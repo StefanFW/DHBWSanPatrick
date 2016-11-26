b26d09eb25e22363cb67b3685a4767f333020d97
Stefan Schneider <info@software-agentur-schneider.de>
2016-11-26 16:08:53 +0100

    Add SpeedBoat vehicle in future mod

--- src/mods/future/VehicleFactory.java
+++ src/mods/future/VehicleFactory.java
@@ -3,6 +3,7 @@ package mods.future;
 import game.Terrain;
 import game.Vehicle;
 import mods.future.vehicles.Spaceship;
+import mods.future.vehicles.SpeedBoat;
 import mods.future.vehicles.Tardies;
 import mods.future.vehicles.Zeppelin;
 
@@ -10,6 +11,9 @@ public class VehicleFactory extends game.VehicleFactory {
     @Override
     public Vehicle createVehicle(Terrain terrain){
         switch (terrain){
+            case WATER:
+                return new SpeedBoat();
+
             case SKY:
                 return new Zeppelin();
 
